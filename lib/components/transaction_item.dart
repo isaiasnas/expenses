b719d08655729147061098beaaae2e39ab17ee5e
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-15 23:38:55 -0300

    refatoração transaction_item.dart

---
+++ lib/components/transaction_item.dart
@@ -0,0 +1,56 @@
+
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionItem extends StatelessWidget {
+  final Transaction tr;
+  final void Function(String p1) onRemove;
+
+  const TransactionItem({
+    Key key,
+    @required this.tr,
+    @required this.onRemove,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: FittedBox(
+              child: Text('R\$${tr.value.toStringAsFixed(2)}'),
+            ),
+          ),
+          radius: 30,
+        ),
+        title: Text(
+          tr.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        subtitle: Text(
+          DateFormat('d MMM y').format(tr.date),
+        ),
+        trailing: MediaQuery.of(context).size.width < 400
+            ? FlatButton.icon(
+                onPressed: () => onRemove(tr.id),
+                icon: const Icon(Icons.delete),
+                label: const Text('Excluir'),
+                textColor: Theme.of(context).errorColor,
+              )
+            : IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () => onRemove(tr.id),
+                color: Theme.of(context).errorColor,
+              ),
+      ),
+    );
+  }
+}
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,6 +1,6 @@
+import 'package:expenses/components/transaction_item.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -35,43 +35,7 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text('R\$${tr.value.toStringAsFixed(2)}'),
-                      ),
-                    ),
-                    radius: 30,
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(
-                    DateFormat('d MMM y').format(tr.date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width < 400
-                      ? FlatButton.icon(
-                          onPressed: () => onRemove(tr.id),
-                          icon: const Icon(Icons.delete),
-                          label: const Text('Excluir'),
-                          textColor: Theme.of(context).errorColor,
-                        )
-                      : IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () => onRemove(tr.id),
-                          color: Theme.of(context).errorColor,
-                        ),
-                ),
-              );
+              return TransactionItem(tr: tr, onRemove: onRemove);
             },
           );
   }
